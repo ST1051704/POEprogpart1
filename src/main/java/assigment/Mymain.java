fe313fe126f0c057ab39b02d2ab0ba89f22c469a
phutimasehela0-arch <phutimasehela0@gmail.com>
2026-09-18 19:00:30 +0200

    i just arrived NIGGA

---
+++ src/main/java/assigment/Mymain.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assigment;
+/**
+ *
+ * @author phuti
+ */
+public class Mymain {
+    
+     public static void main(String[] args) {
+    
+}
+}
